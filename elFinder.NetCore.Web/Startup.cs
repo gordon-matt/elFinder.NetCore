1749f4e7c9d61fb1eb82eb8b6e750edce7e3f17a
Emiliano Sandler <emiliano@inzonesoft.com>
2019-11-14 19:38:40 -0300

    Added compatibility with ASP.NET Core 3.0

--- elFinder.NetCore.Web/Startup.cs
+++ elFinder.NetCore.Web/Startup.cs
@@ -1,10 +1,10 @@
-﻿using System.IO;
-using elFinder.NetCore.Drivers.AzureStorage;
+﻿using elFinder.NetCore.Drivers.AzureStorage;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace elFinder.NetCore.Web
 {
@@ -33,12 +33,12 @@ namespace elFinder.NetCore.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -56,12 +56,11 @@ namespace elFinder.NetCore.Web
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseRouting();
 
-            app.UseMvc(routes =>
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
 
             WebRootPath = env.WebRootPath;
